46c95895db8d41bdd0952c4865719e54e0e53ec2
nunkAlfath <nunkalfath@gmail.com>
2021-07-13 09:53:36 +0700

    drawer

--- App.js
+++ App.js
@@ -1,117 +1,111 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons';
 
-import DetailsScreen from './src/screens/DetailsScreen';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
 import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 import ExploreScreen from './src/screens/ExploreScreen';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-// function HomeScreen(props) {
-//   const {navigation} = props;
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Go to Details"
-//         onPress={() => {
-//           navigation.navigate('Details');
-//           console.log('Tombol tertekan');
-//         }}
-//       />
-//     </View>
-//   );
-// }
+const HomeStackScreen = ({navigation}) => (
+  <HomeStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#008397',
+      },
+      headerTintColor: 'white',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+    }}>
+    <HomeStack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        title: 'Overview',
+        headerleft: () => (
+          <Icon.Button
+            name="close"
+            size={20}
+            backgroundColor="white"
+            onPress={() => {
+              navigation.openDrawer();
+            }}></Icon.Button>
+        ),
+      }}
+    />
+  </HomeStack.Navigator>
+);
 
-// function DetailsScreen({navigation}) {
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       <Text>Details Screen</Text>
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() => {
-//           navigation.push('Details');
-//           console.log('Details Again');
-//         }}
-//       />
-//       <Button
-//         title="Go to Home"
-//         onPress={() => {
-//           navigation.navigate('Home');
-//           console.log('Kembali ke Home');
-//         }}
-//       />
-//       <Button
-//         title="Go Back"
-//         onPress={() => {
-//           navigation.goBack();
-//           console.log('Kembali ke sebelumnya');
-//         }}
-//       />
-//       <Button
-//         title="Go to the first screen"
-//         onPress={() => {
-//           navigation.popToTop();
-//           console.log('Kembali ke halaman 1');
-//         }}
-//       />
-//     </View>
-//   );
-// }
+const DetailsStackScreen = ({navigation}) => (
+  <DetailsStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#008397',
+      },
+      headerTintColor: 'white',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+    }}>
+    <DetailsStack.Screen
+      name="Details"
+      component={DetailsScreen}
+      options={{
+        headerleft: () => (
+          <Icon.Button
+            name="award"
+            size={20}
+            backgroundColor="#008397"
+            onPress={() => {
+              navigation.openDrawer();
+            }}></Icon.Button>
+        ),
+      }}
+    />
+  </DetailsStack.Navigator>
+);
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#008397',
-          },
-          headerTintColor: 'white',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            textAlign: 'center',
-          },
-        }}>
-        <Stack.Screen
-          name="Home"
-          component={DrawerNavigation}
-          options={{
-            title: 'Overview',
-          }}
-        />
-
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStackScreen} />
+        <Drawer.Screen name="Details" component={DetailsStackScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
 
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="Explore" component={ExploreScreen} />
-    </Tab.Navigator>
-  );
-};
+// const TabNavigator = () => {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} />
+//       <Tab.Screen name="Settings" component={SettingsScreen} />
+//       <Tab.Screen name="Explore" component={ExploreScreen} />
+//     </Tab.Navigator>
+//   );
+// };
 
-const DrawerNavigation = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Details" component={DetailsScreen} />
-    </Drawer.Navigator>
-  );
-};
+// const DrawerNavigation = () => {
+//   return (
+//     <Drawer.Navigator>
+//       <Drawer.Screen name="Home" component={HomeScreen} />
+//       <Drawer.Screen name="Details" component={DetailsScreen} />
+//     </Drawer.Navigator>
+//   );
+// };
