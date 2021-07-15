86759ea6543128b54a6e4a3569b40d58945dc0f4
nunkAlfath <nunkalfath@gmail.com>
2021-07-15 21:38:34 +0700

    update login

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,19 +1,120 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {grey100} from 'react-native-paper/lib/typescript/styles/colors';
+import LinearGradient from 'react-native-linear-gradient';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const SplashScreen = ({navigation}) => {
+const SplashScreen = () => {
   return (
-    <View>
-      <Text>Splash Screen</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => {
-          navigation.navigate('Home');
-          console.log('Kembali ke Home');
-        }}
-      />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>header</Text>
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={styles.logo}
+          resizeMode="stretch"
+        />
+        <Text style={{fontSize: 8, color: 'white'}}>
+          fish clipart png from pngtree.com
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.title}>Stay connected with everyone!</Text>
+        <Text style={styles.text}>Sign in with account</Text>
+        <View style={styles.button}>
+          <TouchableOpacity
+            onPress={() => {
+              alert('Click!');
+            }}>
+            <LinearGradient
+              colors={['#08d4c4', '#01ab9d']}
+              style={styles.signIn}>
+              <Text style={styles.textSign}>Get Started!</Text>
+              <MaterialCommunityIcons name="skip-next" color="#fff" size={20} />
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* <Text>Splash Screen</Text>
+      <Button title="Click Here" onPress={() => alert('Button Clicked!')} /> */}
     </View>
   );
 };
 
 export default SplashScreen;
+
+const {height} = Dimensions.get('screen');
+const height_logo = height * 0.28;
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+  container: {
+    flex: 1,
+    // alignItems: "stretch",
+    // justifyContent: 'center',
+    backgroundColor: '#008397',
+  },
+  header: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
+  },
+  title: {
+    color: 'grey',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'grey',
+    marginTop: 5,
+  },
+  button: {
+    alignItems: 'flex-end',
+    marginTop: 30,
+  },
+  signIn: {
+    width: 150,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    flexDirection: 'row',
+  },
+  textSign: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
