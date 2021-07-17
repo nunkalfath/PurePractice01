a7f62a73cf26a90f49854f6e2579b9e6d0b9ff9f
nunkAlfath <nunkalfath@gmail.com>
2021-07-17 20:38:38 +0700

    fixed

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
-import MainTabScreen from './src/screens/MainTabScreen';
-import SupportScreen from './src/screens/SupportScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import BookmarkScreen from './src/screens/BookmarkScreen';
 import RootStackScreen from './src/screens/RootStackScreen';
-import InungScreen from './src/screens/InungScreen';
-
-import {DrawerContent} from './src/screens/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
@@ -18,7 +10,6 @@ const App = () => {
   return (
     <NavigationContainer>
       <RootStackScreen />
-
       {/* <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
         <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
         <Drawer.Screen name="Support" component={SupportScreen} />
